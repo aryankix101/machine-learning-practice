ca0e54d3451aa8029e7590b7df3516f3f8cdd658
Aryan <aryankumawat33@gmail.com>
2020-07-27 10:57:51 -0400

    Finished neural networks

--- Neural_Networks/neural_network.py
+++ Neural_Networks/neural_network.py
@@ -3,4 +3,30 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = keras.datasets
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress,', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+predictions = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i])
+    plt.xlabel("Actual: " + class_names[test_labels[i]])
+    plt.title("Predicion: " + class_names[np.argmax(predictions[i])])
+    plt.show()
