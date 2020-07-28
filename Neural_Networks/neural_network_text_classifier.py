55aa583ca1272c6cf4f4957a46f9c342de2ca949
Aryan <aryankumawat33@gmail.com>
2020-07-28 11:32:52 -0400

    Finished text-classification neural netowrk

--- Neural_Networks/neural_network_text_classifier.py
+++ Neural_Networks/neural_network_text_classifier.py
@@ -1,10 +1,10 @@
-import tensorflow as td
+import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 word_index = data.get_word_index()
 
@@ -16,19 +16,72 @@ word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
-train_data = keras.preprocessing.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
-test = keras.preprocessing.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+test = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
 # model down here
-
+'''
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.Embedding(88000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
+model.summary() 
 
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_labels[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+test_data_1 = np.empty_like(test_data, dtype=object)
+test_labels_1 = np.empty_like(test_data, dtype=object)
+
+results = model.evaluate(test_data_1, test_labels_1)
+print(results)
+
+model.save("model.h5")
+'''
+
+def encode_review(s):
+    encoded = [1]
+
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+            
+    return encoded
+
+model = keras.models.load_model("model.h5")
+
+with open("test.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"", "").strip().split(" ")
+        encode = encode_review(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print("Prediction: " + str(predict[0]))
+
+
+'''
+test_review = test_data[0]
+predict = model.predict([test_review])
+print("Review: ")
+print(decode_review(test_review))
+print("Prediction: " + str(predict[0]))
+print("Actual: " + str(test_labels[0]))
+print(results)
+'''
